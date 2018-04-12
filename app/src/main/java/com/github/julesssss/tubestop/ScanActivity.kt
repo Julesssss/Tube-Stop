dcb1ae0e3cbdcdf9264f7ab91ef4a26aae8e5453
Jules <julianrosser91@gmail.com>
2018-04-12 21:02:04 +0100

    recreate scan page as Kotlin activity

--- app/src/main/java/com/github/julesssss/tubestop/ScanActivity.kt
+++ app/src/main/java/com/github/julesssss/tubestop/ScanActivity.kt
@@ -1,39 +1,26 @@
 package com.github.julesssss.tubestop
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import com.github.julesssss.tubestop.extensions.log
+import com.github.julesssss.tubestop.extensions.toast
 import kotlinx.android.synthetic.main.activity_scan.*
 
-class ScanActivity : AppCompatActivity() {
+class ScanActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // basic Button For searching
+        fab.setOnClickListener {
+            attemptScan()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_scan, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun attemptScan() {
+        // test extensions
+        toast("FAB clicked")
+        log("FAB clicked")
     }
 }
