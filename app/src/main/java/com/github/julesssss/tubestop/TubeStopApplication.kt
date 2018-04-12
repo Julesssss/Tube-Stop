3b7d99bc1ef331ea1cc740c6ccdd8fba1bbf344a
Jules <julianrosser91@gmail.com>
2018-04-12 23:08:48 +0100

    create application class

---
+++ app/src/main/java/com/github/julesssss/tubestop/TubeStopApplication.kt
@@ -0,0 +1,11 @@
+package com.github.julesssss.tubestop
+
+import android.app.Application
+
+class TubeStopApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
