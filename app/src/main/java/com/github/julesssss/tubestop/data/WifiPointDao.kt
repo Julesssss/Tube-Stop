84d1062453856915ec445a79f442a6cbfd3290da
Jules <julianrosser91@gmail.com>
2018-04-15 13:41:46 +0100

    add dao function for retrieving all matching wifipoints

--- app/src/main/java/com/github/julesssss/tubestop/data/WifiPointDao.kt
+++ app/src/main/java/com/github/julesssss/tubestop/data/WifiPointDao.kt
@@ -14,8 +14,11 @@ interface WifiPointDao {
     @Query("SELECT * FROM wifi_points WHERE station == :station")
     fun findWifiPointsForStation(station: String): List<WifiPoint>
 
-    @Query("SELECT * FROM wifi_points WHERE bssid == :bssid")
+    @Query("SELECT * FROM wifi_points WHERE bssid == :bssid LIMIT 1")
     fun findWifiPoint(bssid: String): WifiPoint
+
+    @Query("SELECT * FROM wifi_points WHERE bssid IN (:bssid)")
+    fun findWifiPoints(bssid: List<String>): List<WifiPoint>
 
     @Delete
     fun deleteWifiPoints(wifiPoints: List<WifiPoint>)
