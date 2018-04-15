ff4297049ef79bad2bbbddcd388e9c5a9d156257
Jules <julianrosser91@gmail.com>
2018-04-15 13:01:27 +0100

    refactor WifiPoint class to data class

--- app/src/main/java/com/github/julesssss/tubestop/data/WifiPoint.kt
+++ app/src/main/java/com/github/julesssss/tubestop/data/WifiPoint.kt
@@ -4,16 +4,4 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "wifi_points")
-class WifiPoint() {
-
-
-    @PrimaryKey
-    var bssid :String = ""
-
-    var ssid: String = ""
-
-    var timestamp: Long = 0
-
-    var station: String = ""
-
-}
+data class WifiPoint(@PrimaryKey var bssid: String = "", var ssid: String = "", var timestamp: Long = 0, var station: String = "")
