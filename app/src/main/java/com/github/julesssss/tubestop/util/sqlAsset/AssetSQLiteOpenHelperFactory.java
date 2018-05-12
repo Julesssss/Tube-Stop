3caeca2140d0fa9bd39f0783b6a3c20824f56ceb
Jules <julianrosser91@gmail.com>
2018-05-11 19:09:45 -0700

    add helper utils to enable use of prebuilt room database

---
+++ app/src/main/java/com/github/julesssss/tubestop/util/sqlAsset/AssetSQLiteOpenHelperFactory.java
@@ -0,0 +1,19 @@
+package com.github.julesssss.tubestop.util.sqlAsset;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+
+/**
+ * Implements {@link SupportSQLiteOpenHelper.Factory} using the SQLite implementation in the
+ * framework.
+ */
+@SuppressWarnings("unused")
+public class AssetSQLiteOpenHelperFactory implements SupportSQLiteOpenHelper.Factory {
+
+    @Override
+    public SupportSQLiteOpenHelper create(SupportSQLiteOpenHelper.Configuration configuration) {
+        return new AssetSQLiteOpenHelper(
+                configuration.context, configuration.name, null,
+                1, dbObj -> {}, configuration.callback
+        );
+    }
+}
